31b33682e17fd3576e62aa3ce6ca3c8fd1c810aa
abddoulaye konto <mahamatkonto24@gmail.com>
2025-03-18 17:44:00 +0100

    creation de footer

--- js/script.js
+++ js/script.js
@@ -5,3 +5,25 @@ const mobileSearch = document.getElementById('mobile-search');
 hamburgerBtn.addEventListener('click', () => {
   mobileSearch.classList.toggle('hidden');
 });
+
+document.getElementById("openModal").addEventListener("click", function() {
+  document.getElementById("signupModal").classList.remove("hidden");
+});
+
+document.getElementById("closeModal").addEventListener("click", function() {
+  document.getElementById("signupModal").classList.add("hidden");
+});
+
+// Vérification des mots de passe
+document.getElementById("signupForm").addEventListener("submit", function(event) {
+  let password = document.getElementById("password").value;
+  let confirmPassword = document.getElementById("confirmPassword").value;
+  let errorMsg = document.getElementById("passwordError");
+
+  if (password !== confirmPassword) {
+      errorMsg.classList.remove("hidden");
+      event.preventDefault(); // Empêche l'envoi du formulaire
+  } else {
+      errorMsg.classList.add("hidden");
+  }
+});
